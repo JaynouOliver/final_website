4c9cb71f14b24819e4add943006e5abb70fcc97c
Subhrakamal Das <jyb2084.usa@gmail.com>
2023-03-06 10:15:52 +0530

    commit

---
+++ src/components/Experience.vue
@@ -0,0 +1,263 @@
+<template>
+  <div>
+    <h2>Experience</h2>
+    <div class="timeline">
+      <div class="year-container">
+        <span class="year">2022</span>
+      </div>
+      <ul>
+        <br><br><br>
+        <li>
+          <div class="content">
+            <a href="https://projectx.cloud/" target="_blank">
+              <h3>ProjectX Cloud Technologies</h3>
+              <h5>Co-founder and Director</h5>
+              <div class="grey">June 2022 - Present</div>
+              <br>
+              <div>
+                - A <strong>Startup India recognized</strong> Cloud Computing startup focusing on creating a whole new Operating System where applications run on the cloud.
+                Pitched to <strong>7+ VCs</strong> and got incubated under <strong>NVIDIA Inception Program</strong>
+                <br><br>
+                - Currently working with the Financial Team.
+              </div>
+
+            </a>
+          </div>
+        </li>
+        <li>
+          <div class="content">
+            <a href="https://cloudsek.com" target="_blank">
+              <h3>Virginia Tech</h3>
+              <h5>Student Hackathon Mentor</h5>
+              <div class="grey">Feb 2022</div>
+              <br>
+              <p>
+                - I was offered the Mentor Position at Virginia Tech, one of the leading engineering institutes in the USA. guided students on the field of Machine Learning.
+                <br><br>
+              </p>
+            </a>
+          </div>
+        </li>
+        <li>
+          <div class="content">
+            <a href="https://cloudsek.com" target="_blank">
+              <h3>Ahmedabad University</h3>
+              <h5>Student Hackathon Mentor</h5>
+              <div class="grey">Jan 2022</div>
+              <br>
+              <p>
+                - Was appointed as the Mentor to guide students in the field of Open Source and Machine Learning in the Hackathon - <strong>Ingenium</strong>.
+                <br><br>
+              </p>
+            </a>
+          </div>
+        </li>
+        <li>
+          <div class="content">
+            <a href="https://cloudsek.com" target="_blank">
+              <h3>Neurologic</h3>
+              <h5>Machine Learning Engineer</h5>
+              <div class="grey">Dec 2022</div>
+              <br>
+              <p>
+                - Worked on <strong> Application Tracking System</strong>, Implemented ML Model using BERT with 90% accuracy
+                <br><br>
+              </p>
+            </a>
+          </div>
+        </li>
+        <br> <br>
+      </ul>
+      <div class="year-container">
+        <span class="year">2021</span>
+      </div>
+      <ul>
+        <br> <br> <br>
+        <li>
+          <div class="content align-left">
+            <a href="https://seclogic.io" target="_blank">
+              <h3>Yale University</h3>
+              <h5>Top 40s in Position in Hackathon</h5>
+              <div class="grey">Dec 2021 - Jan 2022</div>
+              <br>
+              <p>
+                - Worked on an <strong>Ambulance WhatsApp chatbot</strong> that gives you live locatoin, address and contacts of nearby ambulances and hospitals near you accourding to your location.
+              </p>
+            </a>
+          </div>
+        </li>
+      </ul>
+      <div class="green-dot"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Experience",
+}
+</script>
+<style scoped>
+h2 {
+  font-weight: 500;
+  color: white;
+  font-size: 1.8rem;
+  border-bottom: 10px solid green;
+  line-height: 0.4;
+  width: 60px;
+  transition: all ease 0.3s;
+  margin-bottom: 2rem;
+}
+
+strong {
+  font-weight: 500;
+  color: #1DB954;
+}
+
+a {
+  color: white;
+  border: none;
+  text-decoration: none;
+}
+
+.green-dot {
+  width: 16px;
+  height: 16px;
+  background: #1DB954;
+  border-radius: 50%;
+  margin-left: -0.45em;
+}
+
+.year-container {
+  position: absolute;
+  margin: 0 -2.25em;
+  z-index: 9999;
+}
+
+.year {
+  color: white;
+  font-weight: 500;
+  font-size: 1.5rem;
+  background: #1DB954;
+  border-radius: 16px;
+  padding: 0.5rem;
+}
+
+h2:hover {
+  font-size: 1.8rem;
+  font-weight: 500;
+  color: white;
+  border-bottom: 10px solid #1DB954;
+  line-height: 0.4;
+  width: 100px;
+  transition: all ease 0.3s;
+}
+
+div {
+  color: white;
+  font-family: "canada-type-gibson" !important;
+}
+
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: 'canada-type-gibson', sans-serif;
+  perspective: 800px;
+}
+
+.grey {
+  color: #ddd;
+  font-size: 0.9rem;
+}
+
+body {
+  display: flex;
+}
+
+.timeline {
+  width: 100%;
+  background-color: #1a202c;
+  color: #fff;
+  padding: 30px 20px;
+  word-wrap: normal;
+}
+
+.timeline ul {
+  list-style-type: none;
+  border-left: 2px solid white;
+  padding: 10px 5px;
+}
+
+.timeline ul li {
+  padding: 20px 20px;
+  position: relative;
+  cursor: pointer;
+  transition: .5s;
+}
+
+.timeline ul li span {
+  display: inline-block;
+  border-radius: 25px;
+  padding: 2px 5px;
+  font-size: 15px;
+  text-align: center;
+}
+
+.timeline ul li .content h3 {
+  color: #1DB954;
+  font-size: 1.6rem;
+  padding-top: 5px;
+  font-weight: 500;
+  margin-bottom: 0.2rem;
+}
+
+h5 {
+  font-size: 1.2rem;
+  padding-top: 5px;
+  font-weight: 500;
+  margin-bottom: 8px;
+}
+
+.timeline ul li .content p {
+  padding: 5px 0px 15px 0px;
+  font-size: 15px;
+}
+
+.timeline ul li:before {
+  position: absolute;
+  content: '';
+  width: 10px;
+  height: 10px;
+  background-color: lawngreen;
+  border-radius: 50%;
+  left: -11px;
+  top: 28px;
+  transition: .5s;
+  margin: 0;
+}
+
+.timeline ul li:hover {
+  background-color: #1b405e;
+  border-radius: 8px;
+}
+
+.timeline ul li:hover:before {
+  background-color: #0F0;
+  box-shadow: 0px 0px 10px 2px #0F0;
+}
+
+@media (max-width: 300px) {
+  .timeline {
+    width: 100%;
+    padding: 30px 5px 30px 10px;
+  }
+
+  .timeline ul li .content h3 {
+    color: #1DB954;
+    font-size: 15px;
+  }
+
+}
+</style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,268 @@
+<template>
+  <div class="hero">
+    <div class="image-holder">
+      <img src="../assets/me.png" alt="Image of Subhrokomol Das"/>
+    </div>
+    <div class="text">
+      <div class="top-text">
+        <h2>
+          Hi, I'm Subhro! 👋
+        </h2>
+        <h3>
+          I'm a
+          <span><vue-typer
+              :text='["ML Freelancer","Web Developer", "Startup Co-Founder"]'
+              :repeat='Infinity'
+              :shuffle='false'
+              initial-action='typing'
+              :pre-type-delay='70'
+              :type-delay='70'
+              :pre-erase-delay='2000'
+              :erase-delay='250'
+              erase-style='select-all'
+              :erase-on-complete='false'
+              caret-animation='blink'
+          ></vue-typer></span>
+          from <br />Kolkata.
+        </h3>
+      </div>
+      <div class="bottom-text">
+        <p class="b-margin">
+          👉 I'm an ML Engineer Freelancer and have worked on projects related to <a
+            target="_blank"
+            rel="noopener noreferrer"
+            class="green"
+            href="https://vit.ac.in"
+        >Computer Vision, Chatbots and NLP.</a
+        >
+        <br>
+        <br>
+        👉 I am an student at <a
+            target="_blank"
+            rel="noopener noreferrer"
+            class="green"
+            href="https://dpsgurgaon.org"
+        >Academy of Technology, Adisaptagram</a>.
+        </p>
+        <p class="b-margin">
+        👉 I am building my company <span class="green-text">ProjectX Cloud Technolgies</span> that aims to reimagine computing through the power of cloud.
+        </p>
+      </div>
+      <div class="contact">
+        <a href="#contact">
+          <div class="contact-me">Contact Me</div>
+        </a>
+      </div>
+      <br>
+      <p class="center"> View some of my projects <a
+          rel="noopener noreferrer"
+          class="green"
+          href="#projects" style="margin-left:1px;margin-right:1px;"
+      >here</a
+      >.
+      </p>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import {VueTyper} from 'vue-typer'
+
+export default {
+  name: "Hero",
+  components: {
+    VueTyper
+  },
+};
+</script>
+
+<style scoped>
+.color {
+  /* border-bottom: 4px solid #1da7ea; */
+  color: lawngreen;
+}
+
+ul {
+  margin: 0;
+  list-style-type: none;
+  display: flex;
+  flex-direction: row;
+}
+
+li {
+  margin-left: 2rem;
+  padding: 10px;
+  display: flex;
+  border-radius: 8px;
+  justify-content: center;
+  align-items: center;
+  background-color: #1b405e;
+  transition: all ease 0.2s;
+}
+
+li:hover {
+  background-color: #18D26E;
+  transform: translateY(-5px);
+  transition: all ease 0.2s;
+}
+
+::v-deep .typed {
+  color: lawngreen !important;
+}
+
+::v-deep .custom.caret {
+  background-color: green !important;
+}
+
+.fa-twitter {
+  color: #fff;
+}
+
+.fa-instagram {
+  color: #fff;
+}
+
+.fa-facebook {
+  color: #fff;
+}
+
+.green {
+  color: #18D26E;
+  text-decoration: none;
+  font-weight: bolder;
+  margin: 5px;
+  transition: siz;
+}
+
+.green-text {
+  color: rgb(0, 252, 55);
+}
+
+.green:hover {
+  color: lawngreen;
+  text-decoration: none;
+  font-weight: bolder;
+  margin: 5px;
+  transition: siz;
+}
+
+.center {
+  text-align: center;
+}
+
+img {
+  height: 400px;
+  border-radius: 8px;
+}
+
+.b-margin {
+  margin-bottom: 1rem;
+}
+
+.image-holder {
+  height: 125px;
+}
+
+.hero {
+  display: flex;
+  margin-bottom: 2rem;
+}
+
+.text {
+  height: 400px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  color: white;
+  margin-left: 2rem;
+  margin-bottom: 4rem;
+}
+
+a {
+  text-decoration: none;
+}
+
+.contact {
+  margin-top: 2rem;
+}
+
+.contact-me {
+  font-size: 1.1rem;
+  font-weight: 500;
+  background-color: #18D26E;
+  color: white;
+  border-radius: 8px;
+  text-align: center;
+  padding: 10px;
+  vertical-align: center;
+  transition: all ease 0.3s;
+}
+
+.contact-me:hover {
+  transform: translateY(-5px);
+  transition: all ease 0.2s;
+}
+
+h2,
+p {
+  margin: 0;
+}
+
+h2 {
+  font-weight: 500;
+  font-size: 1.8rem;
+}
+
+h3 {
+  font-weight: 480;
+  font-size: 1.4rem;
+}
+
+p {
+  font-weight: 300;
+  font-size: 1rem;
+  line-height: 1.3;
+  /* text-align: justify; */
+}
+
+.fa-linkedin {
+  color: white;
+}
+
+@media screen and (max-width: 1258px) {
+  .hero {
+    flex-direction: column;
+    margin-bottom: 0rem;
+  }
+
+  img {
+    height: 150px;
+    width: auto;
+    clip-path: circle(60px at center);
+    position: relative;
+    top: -20px;
+    margin: 0;
+  }
+
+  .text {
+    margin-left: 0;
+    height: auto;
+    margin-bottom: 0.5rem;
+  }
+
+  h2 {
+    margin-bottom: 1rem;
+    margin-top: 1rem;
+  }
+
+  li {
+    margin-right: 0.5rem;
+    margin-left: 0.5rem;
+  }
+
+  .contact {
+    margin-top: 16px;
+  }
+}
+</style>
